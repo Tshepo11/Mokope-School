b14e3cff214f73cb88191ca2fc6357bc20e64346
Tshepo Kingsley Mola <45383774+Tshepo11@users.noreply.github.com>
2020-08-28 14:34:42 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer-area section_gap">
+        <div class="container" align="center">
+
+    <h2 align="center" style="color:#fff"> Mokope School Management System</h2>
+    <span style="color:#fff" >&copy;<a href="https://www.linkedin.com/in/Mola-tshepo-Kingsley" target="_blank"> Mola Tshepo Kingsley - 2020</a></span>
+        </div>
+    </footer>
